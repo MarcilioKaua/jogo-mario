837d9241031f6ac956a2be3aeaad3214b6d946e5
MarcilioKaua <marciliokaua12@gmail.com>
2022-07-21 18:04:05 -0300

    Jogo finalizado

--- js/script.js
+++ js/script.js
@@ -11,14 +11,29 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
+    console.log('loop')
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
     console.log(marioPosition)
     
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        console.log('loop')
         
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'mario-jump-images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.height = '100px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
     }
 
 }, 10)
